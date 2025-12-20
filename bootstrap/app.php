db44df584886332e68aab6facbc74bb0de891211
FERPULI <fernandezluido8@autonoma.edu.pe>
2025-12-20 04:34:19 -0500

    Fix: Remove API prefix for production hosting

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,10 +10,11 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        apiPrefix: '', // 👈 AGREGA ESTA LÍNEA en tu código local
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+    ->withMiddleware(function (Middleware $middleware) {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
